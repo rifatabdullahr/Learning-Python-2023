19a9750da1bd3660bd39becf10631549be2f4d2c
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-10 20:25:05 +0600

    sub function

--- Python RegEx.py
+++ Python RegEx.py
@@ -671,3 +671,25 @@ print(x)
 '''
 
 
+                                                          ##The sub() Function
+
+#The sub() function replaces the matches with the text of your choice:
+#Replace every white-space character with the number 9:
+'''
+import re
+
+#Replace all white-space characters with the digit "9":
+txt = "I am Rifat.Who are you?"
+
+x = re.sub("\s", "9", txt)
+print(x)
+'''
+##You can control the number of replacements by specifying the count parameter:
+#Replace the first 2 occurrences:
+'''
+import re
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+'''
