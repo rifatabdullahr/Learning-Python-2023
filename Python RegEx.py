965f9260b7dcafe74103b720bf8bc03bb841204a
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-10 18:06:09 +0600

    split function

--- Python RegEx.py
+++ Python RegEx.py
@@ -640,3 +640,34 @@ txt = "The rain in Spain"
 x = re.search("Portugal", txt)
 print(x)
 '''
+
+
+                                                ##The split() Function
+
+# The split() function returns a list where the string has been split at each match:
+#Split at each white-space character:
+'''
+import re
+
+#Split the string at every white-space character:
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+'''
+#
+##You can control the number of occurrences by specifying the maxsplit parameter:
+#Split the string only at the first occurrence:
+'''
+import re
+
+#Split the string at the first white-space character:
+
+txt = "When will you come?"
+
+x = re.split("\s", txt, 1)
+
+print(x)
+'''
+
+
