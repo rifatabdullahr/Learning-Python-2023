c47cbbbdff5681cc433d9fe28737b13d80bbc4bc
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-10 12:20:03 +0600

    sets end

--- Python RegEx.py
+++ Python RegEx.py
@@ -457,3 +457,131 @@ if x:
 else :
     print("No match")
 '''
+
+
+## [a-n]	Returns a match for any lower case character, alphabetically between a and n
+'''
+import re
+
+txt = "i am hasib.I read in class 3.Last month my attendence was 100%"
+
+#Check if the string has any characters between a and n:
+
+x = re.findall("[a-n]", txt)
+print(x)
+
+if x:
+    print("Yes, there is at least one match!")
+else:
+    print("No match")
+    '''
+
+
+## [^arn]	Returns a match for any character EXCEPT a, r, and n
+'''
+import re
+
+txt = "I am hasib.I read in class 3.Last month my attendence was 100%"
+
+#Check if the string has other characters than a, r, or n:
+
+x = re.findall("[^arn]", txt)
+print(x)
+
+if x :
+    print("Yes, there is at least one match!")
+else:
+    print("No match")
+'''
+
+
+## [0123]	Returns a match where any of the specified digits (0, 1, 2, or 3) are present
+'''
+import re
+
+txt = "I am hasib.I read in class 3.Last month my attendence was 100%"
+
+#Check if the string has any 0, 1, 2, or 3 digits:
+
+x = re.findall("[0123]", txt)
+print(x)
+
+if x:
+    print("Yes, there is at least one match!")
+else:
+    print("No match")
+    '''
+
+
+## [0-9]	Returns a match for any digit between 0 and 9
+'''
+import re
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any digits:
+
+x = re.findall("[0-9]", txt)
+print(x)
+
+if x:
+    print("Yes, there is at least one match!")
+else:
+    print("No match")
+    '''
+
+
+## [0-8][0-9]	Returns a match for any two-digit numbers from 00 and 59
+'''
+import re
+
+txt = "He came here at 11:20.8 times before 11:45 AM"
+
+#Check if the string has any two-digit numbers, from 00 to 59:
+
+x = re.findall("[0-8][0-9]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+'''
+
+
+## [a-zA-Z]	Returns a match for any character alphabetically between a and z, lower case OR upper case
+'''
+import re
+
+txt = "I am hasib.I read in class 3.Last month my attendence was 100%"
+
+#Check if the string has any characters from a to z lower case, and A to Z upper case:
+x = re.findall("[a-zA-Z]", txt)
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+  '''
+
+
+## [+]	In sets, +, *, ., |, (), $,{} has no special meaning, so [+] means: return a match for any + character in the string
+'''
+import re
+
+txt = "I am hasib.I read in class 3.Last month my attendence was 100%"
+
+#Check if the string has any + characters:
+
+x = re.findall("[+]", txt)
+print(x)
+
+if x:
+    print("Yes, there is at least one match!")
+else:
+  print("No match")
+  '''
+
+
