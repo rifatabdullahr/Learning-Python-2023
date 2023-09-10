524a22e4c914e542893e124e6d77fe1686cef898
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-10 12:28:26 +0600

    findall

--- Python RegEx.py
+++ Python RegEx.py
@@ -585,3 +585,36 @@ else:
   '''
 
 
+                                                      ## The findall() Function
+
+# The findall() function returns a list containing all matches.
+#Print a list of all matches:
+'''
+import re
+
+#Return a list containing every occurrence of "ai":
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+'''
+#The list contains the matches in the order they are found.
+#If no matches are found, an empty list is returned:
+#Return an empty list if no match was found:
+'''
+import re
+
+#Return a list containing every occurrence of "Portugal":
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+
+if x:
+    print("Yes, there is at least one match!")
+else:
+  print("No match")
+'''
+
+
+## 
