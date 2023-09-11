57f986744ef7bbe075a1ca5ade2be5b7692d5d59
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-11 13:26:36 +0600

    match object

--- Python RegEx.py
+++ Python RegEx.py
@@ -693,3 +693,66 @@ txt = "The rain in Spain"
 x = re.sub("\s", "9", txt, 2)
 print(x)
 '''
+
+
+                                                    ## Match Object
+
+# A Match Object is an object containing information about the search and the result. 
+#Note: If there is no match, the value None will be returned, instead of the Match Object.
+# a search that will return a Match Object:
+'''
+import re
+
+txt = "The rain in Spain"
+
+#The search() function returns a Match object:
+
+x = re.search("ai", txt)
+print(x)
+'''
+
+'''
+##The Match object has properties and methods used to retrieve information about the search, and the result:
+
+## .span() returns a tuple containing the start-, and end positions of the match.
+## .string returns the string passed into the function
+## .group() returns the part of the string where there was a match
+'''
+
+
+## Print the position (start- and end-position) of the first match occurrence.
+## The regular expression looks for any words that starts with an upper case "S":
+'''
+import re
+
+txt = "The rain in Spain"
+
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+'''
+
+
+## Print the string passed into the function:
+'''
+import re
+
+txt = "The rain in Spain"
+#The string property returns the search string:
+x = re.search(r"\bS\w+", txt)
+print(x.string)
+'''
+
+
+##Print the part of the string where there was a match.
+
+##The regular expression looks for any words that starts with an upper case "S":
+'''
+import re
+
+#Search for an upper case "S" character in the beginning of a word, and print the word:
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
+'''
+##If there is no match, the value None will be returned, instead of the Match Object.
