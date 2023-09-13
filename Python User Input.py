0e7cd8e144c15f82371a19b04f9c348de55c0d99
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-13 18:59:46 +0600

    user input

--- Python User Input.py
+++ Python User Input.py
@@ -1,1 +1,17 @@
                                                     ##Python User Input
+#User Input
+#Python allows for user input.
+
+#That means we are able to ask the user for input.
+
+#The method is a bit different in Python 3.6 than Python 2.7.
+
+#Python 3.6 uses the input() method.
+
+#Python 2.7 uses the raw_input() method.
+
+#The following example asks for the username, and when you entered the username, it gets printed on the screen:
+'''
+username  = input("Enter your username:")
+print("This pc is belong to " + username +".")
+'''
