07f588723166ce1b47032f6d0c688174d14998a6
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-13 12:41:00 +0600

    finally

--- Python Try Except.py
+++ Python Try Except.py
@@ -55,4 +55,16 @@ else:
     '''
 
 
-#
+                                             ##Finally
+
+#The finally block, if specified, will be executed regardless if the try block raises an error or not.
+'''
+#The finally block gets executed no matter if the try block raises any errors or not:
+
+try:
+    print(x)
+except:
+    print("Something went wrong")
+finally:
+    print("The 'try and except' has finished")
+    '''
