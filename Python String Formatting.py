68c2b88d2a0a88e2b43a11642ca4a534cb937342
Rifat Abdullah <rifat35-2730@diu.edu.bd>
2023-09-14 14:51:35 +0600

    named index

--- Python String Formatting.py
+++ Python String Formatting.py
@@ -34,3 +34,32 @@ price = 49
 myorder = "I want {} pieces of item number {} for {:.2f} dollars."
 print(myorder.format(quantity, itemno, price))
 '''
+
+
+                                            ##Index Numbers
+
+#You can use index numbers (a number inside the curly brackets {0}) to be sure the values are placed in the correct placeholders:
+'''
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+'''
+
+#Also, if you want to refer to the same value more than once, use the index number:
+'''
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
+'''
+
+
+                                                              ##Named Indexes
+#You can also use named indexes by entering a name inside the curly brackets {carname}, 
+# but then you must use names when you pass the parameter values txt.format(carname = "Ford"):
+'''
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
+'''
